7ca9af3e3cfef6b93408ff6d6b7d9f7ca0f86e96
Sobilo34 <bilalsolih60@gmail.com>
2023-07-13 09:49:26 -0700

    n4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include <stdio.h> {
+/**
+ * main - A program that prints a line with put s function
+ * Return: 0 (Success)
+ */int main(void)
+puts("\Programming is like building a multilingual puzzle");
+return (0);
+}
