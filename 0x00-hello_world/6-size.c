d8fd646f866432e6d770d23a0d0768a1107f653d
Sobilo34 <bilalsolih60@gmail.com>
2023-07-13 10:59:10 -0700

    n4

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line using the printf function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+        char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+printf(
