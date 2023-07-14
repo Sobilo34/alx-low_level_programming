eca189563b94a0ee34413bba79c3fe0d5d5a3c66
Sobilo34 <bilalsolih60@gmail.com>
2023-07-14 10:51:09 -0700

    n0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 /**
  * main - Determines if a number is positive, negative or zero
  *
- * Return Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -23,7 +23,7 @@ int main(void)
 	}
 	else
 	{
-		printf"(%d is negative\n", n);
+		printf("%d is negative\n", n);
 	}
 	return (0);
 }
