2eab51143f44bd14c23ae8f5564ee0804f66e7d7
Sobilo34 <bilalsolih60@gmail.com>
2023-07-14 15:03:55 -0700

    n6

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main Prints the alphabets.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char alp[52] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i _ 0; i < 52; i++)
+	}
+	putchar("\n");
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the lowercase alphabet in reverse.
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch ='z'; ch >= 'a'; ch==)
+	{
+		purchar(ch);
+	}
+	purchar("\n")
+		return (0);
+}
