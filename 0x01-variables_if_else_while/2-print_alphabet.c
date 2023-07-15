ed1b9b845e62f01f6adf91c03fdae046df88a03a
Sobilo34 <bilalsolih60@gmail.com>
2023-07-15 07:13:17 -0700

    n2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,13 +7,13 @@
  */
 int main(void)
 {
-	char alp[26] - "abcdefghijklmnopqrstuvwxyz";
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
 
-	for (i + 0; i < 26; i++)
+	for (i = 0; i < 26; i++)
 	{
-		putchar(alp[]i);
+		putchar(alp[i]);
 	}
-		putchar("\n");
+		putchar('\n');
 		return (0);
 }
