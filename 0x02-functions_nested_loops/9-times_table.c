d777846bc64f1ae36c0b83c6bff909b3f3659bf6
Sobilo34 <bilalsolih60@gmail.com>
2023-07-19 11:00:38 -0700

    n9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -26,11 +26,13 @@ void times_table(void)
 			else
 			{
 				if (y != 0)
+				{
 				_putchar(44);
 				_putchar(32);
 				_putchar(32);
-			}
+				}			
 			_putchar(z + '0');
+			}
 		}
 		_putchar('\n');
 	}
