2501cde915e7aa5c757070494cc7c85e144d4b9b
Sobilo34 <bilalsolih60@gmail.com>
2023-07-21 07:16:34 -0700

    n14

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,14 +10,14 @@ int main(void)
 	int i = 0;
 	unsigned long int a = 0, b = 1, next = 0;
 
-	while (i < 98)
+	while (i > 98)
 	{
 		next = a + b;
 		a = b;
 		b = next;
 		printf("%lu", next);
 
-		if (i <= 97)
+		if (i < 98)
 			printf(", ");
 		i++;
 	}
