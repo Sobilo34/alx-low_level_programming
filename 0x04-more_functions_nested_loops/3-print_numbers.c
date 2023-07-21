4de1b95b15b25142f4b60d58cf8f5bf7bde64700
Sobilo34 <bilalsolih60@gmail.com>
2023-07-21 10:19:23 -0700

    n9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - print 0-9
+ * return: void
+ */
+
+void print_numbers(void)
+{
+    char c;
+
+    for (c = 48; c < 58; c++)
+    {
+        _putchar(c);
+    }
+    _putchar('\n');
+}
