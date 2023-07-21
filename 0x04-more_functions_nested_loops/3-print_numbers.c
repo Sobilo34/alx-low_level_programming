cbcb0b83bbe2855a88f351bcdd3e93d8fd849047
Sobilo34 <bilalsolih60@gmail.com>
2023-07-21 12:12:50 -0700

    n4

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,11 +7,11 @@
 
 void print_numbers(void)
 {
-    char c;
+	char c;
 
-    for (c = 48; c < 58; c++)
-    {
-        _putchar(c);
-    }
-    _putchar('\n');
+	for (c = 48; c < 58; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
 }
