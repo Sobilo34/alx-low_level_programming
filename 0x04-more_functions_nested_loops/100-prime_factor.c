084d1af5803bd452289ecd90fa402e732c7b1ac7
Sobilo34 <bilalsolih60@gmail.com>
2023-07-22 08:10:44 -0700

    n11

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -8,32 +8,12 @@
  */
 int main(void)
 {
-	long int n;
-	long int max;
-	long int i;
+       unsigned long int i, n = 612852475143;
 
-	n = 6122852475143;
-	max = -1;
-
-	while (n % 2 == 0)
-	{
-		max = 2;
-		n /= 2;
-	}
-
-	for (i = 3; i <= sqrt(n); i = i + 2)
-	{
-		while (n % i == 0)
-		{
-			max = i;
-			n = n / i;
-		}
-	}
-
-	if (n > 2)
-		max = n;
-
-	printf("%ld\n", max);
-
-	return (0);
-}
+       for (i = 3; i < 782849); i = i + 2)
+       {
+	       while ((n % i == 0) && (n != i))
+		       n = n / i;
+       }
+       printf("%lu\n", n);
+       return (0);
