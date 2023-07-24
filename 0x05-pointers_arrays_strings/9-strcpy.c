ba71b22af26e85bb388c76ee4f4e168797905e98
Sobilo34 <bilalsolih60@gmail.com>
2023-07-24 09:14:03 -0700

    n9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies the string pointed to by scr
+ * @dest: destination
+ * @scr: source
+ * Return: string
+ */
+char *_strcpy(char *dest, char *scr)
+{
+	int len = 0;
+
+	while (*(src + len) != '\0')
+	{
+		*(dest + len) = *(scr + len);
+		len++;
+	}
+
+	*(dest + len) = '\0';
+	return (dest);
+}
