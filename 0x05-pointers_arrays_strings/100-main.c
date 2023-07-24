ad056e8871f3c5ff13b1ba55ab3d368bf0fea786
Sobilo34 <bilalsolih60@gmail.com>
2023-07-24 12:14:07 -0700

    n10

--- 0x05-pointers_arrays_strings/100-main.c
+++ 0x05-pointers_arrays_strings/100-main.c
@@ -3,6 +3,7 @@
 
 /**
  * main - check the code
+ *
  * Return: Always 0.
  */
 int main(void)
@@ -13,15 +14,15 @@ int main(void)
 	printf("%d\n", nb);
 	nb = _atoi("-402");
 	printf("%d\n", nb);
-	nb = _atoi("          -------++++++-----+++++--98");
+	nb = _atoi("          ------++++++-----+++++--98");
 	printf("%d\n", nb);
 	nb = _atoi("214748364");
 	printf("%d\n", nb);
 	nb = _atoi("0");
 	printf("%d\n", nb);
-	nb = _atoi("Suite 402");
+ 	nb = _atoi("Suite 402");
 	printf("%d\n", nb);
-	nb = _atoi(" +      +  -  -98 Battery Street; San Francisco, CA 94111 - USA");
+	nb = _atoi("         +      +    -    -98 Battery Street; San Francisco, CA 94111 - USA             ");
 	printf("%d\n", nb);
 	nb = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
 	printf("%d\n", nb);
