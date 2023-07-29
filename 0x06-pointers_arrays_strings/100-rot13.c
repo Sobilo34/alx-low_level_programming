8387a0d42eae3663c3bf9754a005b9a80c23d135
Sobilo34 <bilalsolih60@gmail.com>
2023-07-29 01:01:15 -0700

    n8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -11,16 +11,16 @@ char *rot13(char *s)
 	int i;
 	int j;
 	char data1[] =
-		"ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char datarot[] =
 		"NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; j < 52; j++)
 		{
-			if (s[i] == data[j])
+			if (s[i] == data1[j])
 			{
-				s[i] == data[j];
+			s[i] = datarot[j];
 				break;
 			}
 		}
