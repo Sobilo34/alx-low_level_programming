c5e9692c047c6c7ce81bd9cdc50f6f4684956021
Sobilo34 <bilalsolih60@gmail.com>
2023-07-31 10:20:16 -0700

    n8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -3,19 +3,18 @@
 
 /**
  * print_square - prints a square, followed by a new line
- * @size: sizwe of the square
+ * @size: size of the square
+ * Return: sqaure msde of "#"
  */
-void print_sqaure(int size)
+void print_square(int size)
 {
 	int i, j;
 
 	if (size <= 0)
-	{
 		_putchar('\n');
-	}
 	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < size; j++)
+		for (j = 0; j < (size); j++)
 		{
 			_putchar('#');
 		}
