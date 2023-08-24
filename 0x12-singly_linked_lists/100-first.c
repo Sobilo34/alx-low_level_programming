4f73c9934563d2dd59aedf9f76426664e7dcf684
root <bilalsolih60@gmail.com>
2023-08-24 11:43:55 -0700

    n4

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,8 +6,8 @@ void first(void) __attribute__ ((constructor));
 * first - prints a sentence before the main
 * function is executed
 */
-void first(void0
+void first(void)
 {
-printf("You're beat! and yet, you must allow,\n");
-printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
