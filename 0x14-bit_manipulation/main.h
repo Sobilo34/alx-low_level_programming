b8c8696b6ec72fcb42664b41e23af8e6b2b5cdd4
Sobilo34 <bilalsolih60@gmail.com>
2023-09-28 07:27:55 -0700

    2-get_bit.c

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdlib.h>
+
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
