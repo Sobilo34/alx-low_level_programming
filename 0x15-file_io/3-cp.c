039231c50ac32950cb844d32e6551d3b9a02429f
Sobilo34 <bilalsolih60@gmail.com>
2023-10-03 03:11:41 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	bufa = create_bufa(argv[2]);
