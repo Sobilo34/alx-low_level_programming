119e03018d8e2b9bc801612dce1d7a09ecc921a9
Sobilo34 <bilalsolih60@gmail.com>
2023-10-30 06:05:50 -0700

    100-elf_header.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,11 +8,23 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
+#include <elf.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 void error_and_exit(int a, const char *format, const char *arg);
+void elf_check(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+void elf_close(int elf);
+
 
 #endif
