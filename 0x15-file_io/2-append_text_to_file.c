a691400ab3437a5092540d0cdc68d380b2feacaa
Sobilo34 <bilalsolih60@gmail.com>
2023-10-30 06:31:05 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -19,7 +19,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	if (access(filename, X_OK) == -1)
+	if (access(filename, F_OK) == -1)
 	{
 		return (-1);
 	}
