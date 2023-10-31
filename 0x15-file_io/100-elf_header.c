471c11c870b2c010ffca16de5fe2205598594e84
Sobilo34 <bilalsolih60@gmail.com>
2023-10-31 01:47:34 -0700

    100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -25,14 +25,16 @@ void display_info(const Elf64_Ehdr *elf_header)
 	printf("  Magic:   ");
 	for (i = 0; i < EI_NIDENT; i++)
 	{
-		printf("%02x ", elf_header->e_ident[i]);
+		printf("%02x", elf_header->e_ident[i]);
 		if (i < EI_NIDENT - 1)
 			printf(" ");
 	}
 	printf("\n");
 
-	printf("  Class:                             %s\n",
+
+	printf("  Class:                            %s\n",
 	       elf_header->e_ident[EI_CLASS] == ELFCLASS64 ? "ELF64" : "ELF32");
+
 	printf("  Data:                              %s\n",
 	       elf_header->e_ident[EI_DATA] == ELFDATA2LSB
 		   ? "2's complement, little endian"
@@ -40,16 +42,16 @@ void display_info(const Elf64_Ehdr *elf_header)
 	printf("  Version:                           %d (current)\n",
 	       elf_header->e_ident[EI_VERSION]);
 	printf("  OS/ABI:                            %s\n",
-	       get_os_abi(elf_header->e_ident[EI_OSABI]));
+			get_os_abi(elf_header->e_ident[EI_OSABI]));
 
 	printf("  ABI Version:                       %d\n",
 	       elf_header->e_ident[EI_ABIVERSION]);
 
 	printf("  Type:                              %s\n",
-	       get_type_string(elf_header->e_type));
+			get_type_string(elf_header->e_type));
 
 	printf("  Entry point address:               0x%lx\n",
-	       (unsigned long)elf_header->e_entry);
+	       (unsigned long)elf_header->e_entry & 0xFFFFFFFF);
 }
 
 /**
