b2421ca4bf9a66286adbb5563b1e98e7d11be093
Sobilo34 <bilalsolih60@gmail.com>
2023-10-31 03:57:02 -0700

    3-cp.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -14,6 +14,7 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void check_file(int file_from, int file_to, char *argv[]);
 void elf_check(unsigned char *e_ident);
 void print_magic(unsigned char *e_ident);
 void print_class(unsigned char *e_ident);
