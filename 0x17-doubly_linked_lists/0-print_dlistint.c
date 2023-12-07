4dffcc8a56c562a83bf1c8677ccd5961020dfd6b
Sobilo34 <bilalsolih60@gmail.com>
2023-12-07 08:53:19 -0800

    0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,7 +2,7 @@
 /**
   * print_dlistint - THis is a function that prints a doubly linked list
   * @h: THe head of the list to be printed
-  * Return: Number of items in the list 
+  * Return: Number of items in the list
   */
 size_t print_dlistint(const dlistint_t *h)
 {
