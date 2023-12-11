0ffd6fc16cc3b45b340c603d512355848d394d56
Sobilo34 <bilalsolih60@gmail.com>
2023-12-11 05:43:32 -0800

    A function that Create a dynamic library that contains C functions that can be called from Python

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,77 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers
+ * @a: First integer
+ * @b: Second integer
+ * Return: Sum of a and b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - Subtracts two integers
+ * @a: First integer
+ * @b: Second integer
+ * Return: Result of a minus b
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - Multiplies two integers
+ * @a: First integer
+ * @b: Second integer
+ * Return: Product of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - Divides two integers
+ * @a: First integer
+ * @b: Second integer
+ * Return: Result of a divided by b
+ */
+
+int sb_div(int a, int b)
+{
+	if (b != 0)
+	{
+		return (a / b);
+	}
+	else
+	{
+		printf("Error: Division by zero\n");
+		return (0);
+	}
+}
+
+/**
+ * mod - Computes the remainder of the division of two integers
+ * @a: First integer
+ * @b: Second integer
+ * Return: Remainder of the division of a by b
+ */
+
+int mod(int a, int b)
+{
+	if (b != 0)
+	{
+		return (a % b);
+	}
+	else
+	{
+		printf("Error: Modulus by zero\n");
+		return (0);
+	}
+}
