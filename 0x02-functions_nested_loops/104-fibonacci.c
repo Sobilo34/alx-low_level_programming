733761b29391b2e2c09cd3d5d8194c8c5145b8f5
Sobilo34 <bilalsolih60@gmail.com>
2024-02-20 18:29:38 -0800

    corrected mistake in conditional statement

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
  * main - main block
- * Description: computes and prints the sum of all the multiples of 3 or
- * 5 below 1024 (excluded), followed by a new line
+ * Description: rogram that finds and prints the first 98 Fibonacci
+ * numbers, starting with 1 and 2 followed by a new line
  * Return: 0
  */
 int main(void)
@@ -10,7 +10,7 @@ int main(void)
 	int i = 0;
 	unsigned long int a = 0, b = 1, next = 0;
 
-	while (i > 98)
+	while (i < 98)
 	{
 		next = a + b;
 		a = b;
