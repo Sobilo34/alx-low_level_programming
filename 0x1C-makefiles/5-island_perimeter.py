9dd2f735bcbc699a5ad9e5a61278533d9f7ae590
Sobilo34 <bilalsolih60@gmail.com>
2024-02-21 08:52:54 -0800

    function def island_perimeter(grid): that returns the perimeter of the island described in grid

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""Returns the perimeter of the island described in grid"""
+
 
 def island_perimeter(grid):
     """
@@ -10,6 +12,18 @@ def island_perimeter(grid):
 
     Return:
         int: The Perimeter(p) of the Island
+
+    Requirements:
+        grid is a list of list of integers:
+        0 represents a water zone
+        1 represents a land zone
+        One cell is a square with side length 1
+        Grid cells are connected horizontally/vertically (not diagonally).
+        Grid is rectangular, width and height don’t exceed 100
+        Grid is completely surrounded by water
+        there is one island (or nothing).
+        The island doesn’t have “lakes”
+        Prototype: def island_perimeter(grid):
     """
     p = 0
 
