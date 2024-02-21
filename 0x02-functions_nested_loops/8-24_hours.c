3c36a6b01f27f336f56e7b9350c652d131513284
Sobilo34 <bilalsolih60@gmail.com>
2024-02-20 17:56:09 -0800

    change the orientation for calculating the time

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,18 +5,28 @@
  */
 void jack_bauer(void)
 {
-	int hour, min;
+	int a, b, c, d;
 
-	for (hour = 0, hour <= 24 hour++)
+	for (a = 0; a <= 2; a++)
 	{
-		for (min = 0, min < 60, min++)
+		for (b = 0; b <= 9; b++)
 		{
-			_putchar('0' + (hour / 10));
-			_putchar('0' + (hour % 10));
-			_putchar(':');
-			_putchar('0' + (min / 10));
-			_putchar('0' + (min % 10));
-			_putchar('\n');
+			if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
+			{
+				for (c = 0; c <= 5; c++)
+
+				{
+					for (d = 0; d <= 9; d++)
+					{
+						_putchar(a + '0');
+						_putchar(b + '0');
+						_putchar(58);
+						_putchar(c + '0');
+						_putchar(d + '0');
+						_putchar('\n');
+					}
+				}
+			}
 		}
 	}
 }
